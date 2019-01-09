b0ecb1a874065fdfa9c436c6b3163e1ce7403694
AntonVoronezh <pstut@mail.ru>
2019-01-09 09:33:07 +0300

    add skills.json

--- src/assets/scripts/modules/skills.js
+++ src/assets/scripts/modules/skills.js
@@ -1,8 +1,36 @@
 import Vue from 'vue';
 
+
+
+const skill = {
+
+    template: '#skill'
+
+};
+
+const skillsRow = {
+
+    components: { skill },
+
+    template: '#skills-item'
+
+};
+
+
+
 new Vue({
 
     el: "#skills-component",
+
+    components: { skillsRow },
+
+    data() {
+
+        return {
+            skills: {}
+        }
+
+    },
     
     template: "#skills-list"
 
