684e8075ae708e9e79067a57871fe9e974c72753
AntonVoronezh <pstut@mail.ru>
2019-01-17 17:13:29 +0300

    blur

--- src/assets/scripts/modules/scrollBlur.js
+++ src/assets/scripts/modules/scrollBlur.js
@@ -1,62 +1,20 @@
 export default function scrollBlur() { 
 
-    const wrapper = document.querySelector('.comments__wrap');
-    const blur = document.querySelector('.comments__back');
+  let wrapper = document.getElementById('js_blur-wrap');
+  let blur = document.getElementById('js_blur');
   
     return {
       set() {
-        const imgWidth = document.querySelector('.comments').offsetWidth,
-         imgWidthBack = document.querySelector('.comments__back').offsetWidth,
-        //   posLeft = -wrapper.offsetLeft,
-        //   posTop = -wrapper.offsetTop,
-          posLeft = -wrapper.offsetLeft,
-          posTop = -wrapper.offsetTop,
-          blurCSS = blur.style;
+        let imgWidth = document.getElementById('js_blur-container').offsetWidth,
+        posLeft = -wrapper.offsetLeft,
+        posTop = -wrapper.offsetTop,
+        blurCSS = blur.style;
 
-          console.log(imgWidth, imgWidthBack);
-        //   console.log(-wrapper.offsetLeft, -wrapper.offsetTop);
-  
+
         blurCSS.backgroundSize = `${imgWidth}px`;
         blurCSS.backgroundPosition = `${posLeft}px ${posTop}px`;
       }
     };
-
-
-    // const wrapper = document.querySelector(".comments__wrap");
-    // const form = document.querySelector(".comments__back");
-  
-    // return {
-    //   set: function() {
-    //     const posLeft = -wrapper.offsetLeft;
-    //     const posTop = -wrapper.offsetTop;
-    //     const blurCss = form.style;
-  
-    //     blurCss.backgroundPosition = `${posLeft}px ${posTop}px`;
-    //   }
-    // };
-
-    // Схематичная разметка для понимания
-    // section.b-works-what
-    //     form.b-works-form
-    //         .b-works-form__blur
-
-    // const container = document.querySelector('.b-works-form');
-    // const sectionEl = document.querySelector('.b-works-what');
-    // const blurEl = document.createElement('.b-works-form__blur');
-    
-    // const drawBlur = function () {
-    //   const width = sectionEl.offsetWidth;
-    //   const height = sectionEl.offsetHeight;
-    //   const left = container.offsetLeft;
-    //   const bottom = height - (container.offsetTop + container.offsetHeight);
-    
-    //   blurEl.style.width = width + "px";
-    //   blurEl.style.height = height + "px";
-    //   blurEl.style.left = `-${left}px`;
-    //   blurEl.style.bottom = `-${bottom}px`;
-    // };
-    
-
 
 
 }
