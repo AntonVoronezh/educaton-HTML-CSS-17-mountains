c9eb85a70b8cfb770b581e7d2c6c49cb24999862
AntonVoronezh <pstut@mail.ru>
2019-01-17 17:39:42 +0300

    fix

---
+++ src/assets/scripts/blur.js
@@ -0,0 +1,20 @@
+export default function scrollBlur() { 
+
+    let wrapper = document.getElementById('js_blur-wrap');
+    let blur = document.getElementById('js_blur');
+    
+      return {
+        set() {
+          let imgWidth = document.getElementById('js_blur-container').offsetWidth,
+          posLeft = -wrapper.offsetLeft,
+          posTop = -wrapper.offsetTop,
+          blurCSS = blur.style;
+  
+  
+          blurCSS.backgroundSize = `${imgWidth}px`;
+          blurCSS.backgroundPosition = `${posLeft}px ${posTop}px`;
+        }
+      };
+  
+  
+  }
